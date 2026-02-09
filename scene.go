20a2f65852aa01cb12133876c233d6abbaecca09
Alvino Barboza <alvinobarboza@gmail.com>
2026-02-09 13:55:29 -0300

    Scene holds pointers to models

--- scene.go
+++ scene.go
@@ -2,17 +2,17 @@ package main
 
 type Scene struct {
 	activeCam *Camera
-	objects   []Mesh
+	objects   []*Model
 }
 
 func NewScene(c *Camera) Scene {
 	return Scene{
 		activeCam: c,
-		objects:   make([]Mesh, 0),
+		objects:   make([]*Model, 0),
 	}
 }
 
-func (s *Scene) AddMesh(o Mesh) {
+func (s *Scene) AddMesh(o *Model) {
 	s.objects = append(s.objects, o)
 }
 
