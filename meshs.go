b0c1ab63c3aa46a3271293c347acae5c17f3d612
Alvino Barboza <alvinobarboza@gmail.com>
2026-02-09 13:54:24 -0300

    Calculate sphere bounds

--- meshs.go
+++ meshs.go
@@ -7,9 +7,40 @@ type Mesh struct {
 	color    color.RGBA
 }
 
-func NewMesh(v []Vec3, color color.RGBA) Mesh {
-	return Mesh{
-		vertices: v,
-		color:    color,
+
+type BoundingSphere struct {
+	center, centerWord Vec3
+	radius             float32
+}
+
+func NewBoundingSphere() BoundingSphere {
+	return BoundingSphere{
+		center:     NewVec3(0, 0, 0),
+		centerWord: NewVec3(0, 0, 0),
+		radius:     0,
 	}
+}
+
+func (s *BoundingSphere) CalculateBoundaries(verts []Vec3, scale Matrix) {
+	*s = NewBoundingSphere()
+
+	for _, v := range verts {
+		s.center = s.center.Add(v)
+	}
+
+	s.center = s.center.Divide(float32(len(verts)))
+
+	for _, v := range verts {
+		scaled := scale.MultiplyByVec3(v)
+		scaled = scaled.Sub(s.center)
+
+		r := scaled.Length()
+
+		if s.radius < r {
+			s.radius = r
+		}
+	}
+}
+
+	boundingSphere BoundingSphere
 }
