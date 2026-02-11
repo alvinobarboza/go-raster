76f54ca209e3b61ddabdd54784d1e2608ba629f2
Alvino Barboza <alvinobarboza@gmail.com>
2026-02-11 12:45:33 -0300

    Load mesh from OBJ file

--- mesh_loader.go
+++ mesh_loader.go
@@ -9,6 +9,69 @@ import (
 	"strings"
 )
 
+func LoadVec3(line string, indices int) (float32, float32, float32) {
+	var result [3]float32
+
+	line = strings.Replace(line, "\n", "", 1)
+	line = strings.Replace(line, "\r", "", 1)
+	data := strings.Split(line, " ")
+
+	for i := range indices {
+		d, _ := strconv.ParseFloat(data[i+1], 32)
+		result[i] = float32(d)
+	}
+
+	return result[0], result[1], result[2]
+}
+
+func loadTriangleMetaData(indexes string) (int, int, int) {
+	var vertex, normal, uv int
+
+	idxs := strings.Split(indexes, "/")
+	switch len(idxs) {
+	case 1:
+		vertex, _ = strconv.Atoi(idxs[0])
+	case 2:
+		vertex, _ = strconv.Atoi(idxs[0])
+		uv, _ = strconv.Atoi(idxs[1])
+	case 3:
+		vertex, _ = strconv.Atoi(idxs[0])
+		uv, _ = strconv.Atoi(idxs[1])
+		normal, _ = strconv.Atoi(idxs[2])
+	}
+
+	return vertex - 1, normal - 1, uv - 1
+}
+
+func LoadTriangle(line string) []Triangle {
+	tris := make([]Triangle, 0)
+
+	line = strings.Replace(line, "\n", "", 1)
+	line = strings.Replace(line, "\r", "", 1)
+
+	data := strings.Split(line, " ")
+	data = data[1:]
+
+	cursor := 3
+	offset := 0
+	origin := data[0]
+	for {
+		tmpData := data[offset:cursor]
+		t := Triangle{}
+		t.v1, t.n1, t.u1 = loadTriangleMetaData(origin)
+		t.v2, t.n2, t.u2 = loadTriangleMetaData(tmpData[1])
+		t.v3, t.n3, t.u3 = loadTriangleMetaData(tmpData[2])
+		tris = append(tris, t)
+		offset++
+		cursor++
+		if cursor > len(data) {
+			break
+		}
+	}
+
+	return tris
+}
+
 func LoadMeshFromFile(modelPath string, texturePath string) (MeshData, error) {
 	file, err := os.Open(modelPath)
 	if err != nil {
@@ -19,6 +82,9 @@ func LoadMeshFromFile(modelPath string, texturePath string) (MeshData, error) {
 
 	scanner := bufio.NewReader(file)
 	verts := make([]Vec3, 0)
+	normals := make([]Vec3, 0)
+	uvs := make([]Vec3, 0)
+	tris := make([]Triangle, 0)
 
 	for {
 		line, err := scanner.ReadString('\n')
@@ -32,20 +98,28 @@ func LoadMeshFromFile(modelPath string, texturePath string) (MeshData, error) {
 			log.Println(err)
 			return MeshData{}, err
 		}
-		if line[0:2] == "v " {
-			line = strings.Replace(line, "\n", "", 1)
-			line = strings.Replace(line, "\r", "", 1)
-			data := strings.Split(line, " ")
-			x, _ := strconv.ParseFloat(data[1], 32)
-			y, _ := strconv.ParseFloat(data[2], 32)
-			z, _ := strconv.ParseFloat(data[3], 32)
-			verts = append(verts, NewVec3(float32(x), float32(y), float32(z)))
+		if len(line) > 2 {
+			switch line[:2] {
+			case "v ":
+				x, y, z := LoadVec3(line, 3)
+				verts = append(verts, NewVec3(x, y, z))
+			case "vn":
+				x, y, z := LoadVec3(line, 3)
+				normals = append(normals, NewVec3(x, y, z))
+			case "vt":
+				x, y, _ := LoadVec3(line, 2)
+				uvs = append(uvs, NewVec3(x, y, 0))
+			case "f ":
+				t := LoadTriangle(line)
+				tris = append(tris, t...)
+			}
 		}
 	}
 
-	log.Println(verts)
-
 	return MeshData{
-		verts: verts,
+		verts:   verts,
+		normals: normals,
+		uv:      uvs,
+		tris:    tris,
 	}, nil
 }
--- mesh_loader_test.go
+++ mesh_loader_test.go
@@ -1,9 +1,216 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestMeshLoader(t *testing.T) {
-	t.Run("loadFile", func(t *testing.T) {
-		LoadMeshFromFile("./assets/cube.obj", "")
+	t.Run("loadVec3 Vertex", func(t *testing.T) {
+		lineToParse := "v 1.0 1.0 1.0"
+
+		want := NewVec3(1, 1, 1)
+
+		x, y, z := LoadVec3(lineToParse, 3)
+		got := NewVec3(x, y, z)
+
+		if want.X != got.X &&
+			want.Y != got.Y &&
+			want.Z != got.Z {
+			t.Errorf("wanted %+v, got %+v", want, got)
+		}
+	})
+
+	t.Run("loadVec3 UV", func(t *testing.T) {
+		lineToParse := "v 1.0 1.0 0.0"
+
+		want := NewVec3(1, 1, 0)
+
+		x, y, z := LoadVec3(lineToParse, 3)
+		got := NewVec3(x, y, z)
+
+		if want.X != got.X &&
+			want.Y != got.Y &&
+			want.Z != got.Z {
+			t.Errorf("wanted %+v, got %+v", want, got)
+		}
+	})
+
+	t.Run("LoadTriangle Vertex", func(t *testing.T) {
+		lineToParse := "f 2 2 2"
+		want := Triangle{
+			v1: 1,
+			v2: 1,
+			v3: 1,
+		}
+
+		got := LoadTriangle(lineToParse)
+
+		if len(got) != 1 {
+			t.Error("Didn't parse anything")
+		}
+		if got[0].v1 != want.v1 &&
+			got[0].v2 != want.v2 &&
+			got[0].v3 != want.v3 {
+			t.Errorf("Wanted %+v, got %+v", want, got)
+		}
+	})
+
+	t.Run("LoadTriangle Vertex UV", func(t *testing.T) {
+		lineToParse := "f 2/2 2/2 2/2\n\r"
+		want := Triangle{
+			v1: 1,
+			v2: 1,
+			v3: 1,
+			u1: 1,
+			u2: 1,
+			u3: 1,
+		}
+
+		got := LoadTriangle(lineToParse)
+
+		if len(got) != 1 {
+			t.Error("Didn't parse anything")
+		}
+		if got[0].v1 != want.v1 &&
+			got[0].v2 != want.v2 &&
+			got[0].v3 != want.v3 &&
+			got[0].u1 != want.u1 &&
+			got[0].u2 != want.u2 &&
+			got[0].u3 != want.u3 {
+			t.Errorf("Wanted %+v, got %+v", want, got)
+		}
+	})
+
+	t.Run("LoadTriangle Vertex Normal", func(t *testing.T) {
+		lineToParse := "f 2//2 2//2 2//2\n\r"
+		want := Triangle{
+			v1: 1,
+			v2: 1,
+			v3: 1,
+			n1: 1,
+			n2: 1,
+			n3: 1,
+		}
+
+		got := LoadTriangle(lineToParse)
+
+		if len(got) != 1 {
+			t.Error("Didn't parse anything")
+		}
+		if got[0].v1 != want.v1 &&
+			got[0].v2 != want.v2 &&
+			got[0].v3 != want.v3 &&
+			got[0].n1 != want.n1 &&
+			got[0].n2 != want.n2 &&
+			got[0].n3 != want.n3 {
+			t.Errorf("Wanted %+v, got %+v", want, got)
+		}
+	})
+
+	t.Run("LoadTriangle Full 1", func(t *testing.T) {
+		lineToParse := "f 2/3/2 2/3/2 2/3/2\n\r"
+		want := Triangle{
+			v1: 1,
+			v2: 1,
+			v3: 1,
+			u1: 2,
+			u2: 2,
+			u3: 2,
+			n1: 1,
+			n2: 1,
+			n3: 1,
+		}
+
+		got := LoadTriangle(lineToParse)
+
+		if len(got) != 1 {
+			t.Error("Didn't parse anything")
+		}
+		if got[0].v1 != want.v1 &&
+			got[0].v2 != want.v2 &&
+			got[0].v3 != want.v3 &&
+			got[0].n1 != want.n1 &&
+			got[0].n2 != want.n2 &&
+			got[0].n3 != want.n3 &&
+			got[0].u1 != want.u1 &&
+			got[0].u2 != want.u2 &&
+			got[0].u3 != want.u3 {
+			t.Errorf("Wanted %+v, got %+v", want, got)
+		}
+	})
+
+	t.Run("LoadTriangle Full 2", func(t *testing.T) {
+		lineToParse := "f 2/3/4 5/6/7 8/9/10 11/12/13\n\r"
+		want := []Triangle{
+			{
+				v1: 1, u1: 2, n1: 3,
+				v2: 4, u2: 5, n2: 6,
+				v3: 7, u3: 8, n3: 9,
+			},
+			{
+				v1: 1, u1: 2, n1: 3,
+				v2: 7, u2: 8, n2: 9,
+				v3: 10, u3: 11, n3: 12,
+			},
+		}
+
+		got := LoadTriangle(lineToParse)
+
+		if len(got) != 2 {
+			t.Error("Didn't parse anything")
+		}
+		for i := range 2 {
+			if got[i].v1 != want[i].v1 &&
+				got[i].v2 != want[i].v2 &&
+				got[i].v3 != want[i].v3 &&
+				got[i].n1 != want[i].n1 &&
+				got[i].n2 != want[i].n2 &&
+				got[i].n3 != want[i].n3 &&
+				got[i].u1 != want[i].u1 &&
+				got[i].u2 != want[i].u2 &&
+				got[i].u3 != want[i].u3 {
+				t.Errorf("Wanted %+v, got %+v", want, got)
+			}
+		}
+	})
+
+	t.Run("LoadTriangle Full 3", func(t *testing.T) {
+		lineToParse := "f 2/3/4 5/6/7 8/9/10 11/12/13 14/15/16\n\r"
+		want := []Triangle{
+			{
+				v1: 1, u1: 2, n1: 3,
+				v2: 4, u2: 5, n2: 6,
+				v3: 7, u3: 8, n3: 9,
+			},
+			{
+				v1: 1, u1: 2, n1: 3,
+				v2: 7, u2: 8, n2: 9,
+				v3: 10, u3: 11, n3: 12,
+			},
+			{
+				v1: 1, u1: 2, n1: 3,
+				v2: 10, u2: 11, n2: 12,
+				v3: 13, u3: 14, n3: 15,
+			},
+		}
+
+		got := LoadTriangle(lineToParse)
+
+		if len(got) != 3 {
+			t.Error("Didn't parse anything")
+		}
+		for i := range 3 {
+			if got[i].v1 != want[i].v1 &&
+				got[i].v2 != want[i].v2 &&
+				got[i].v3 != want[i].v3 &&
+				got[i].n1 != want[i].n1 &&
+				got[i].n2 != want[i].n2 &&
+				got[i].n3 != want[i].n3 &&
+				got[i].u1 != want[i].u1 &&
+				got[i].u2 != want[i].u2 &&
+				got[i].u3 != want[i].u3 {
+				t.Errorf("Wanted %+v, got %+v", want, got)
+			}
+		}
 	})
 }
